8888a000f3ab5c7a6e4b0eaa90ff99f758d116ee
jeffali <hichame.jeffali@cfengine.com>
2013-09-17 19:30:52 +0000

    Generating random salts

--- libutils/pwcrypt.c
+++ libutils/pwcrypt.c
@@ -1,5 +1,22 @@
 #include <pwcrypt.h>
 
+int getrndsalt(PwHashMethod m, char *obuf) {
+   int r = (int) (( random() % 8L )+ 8);
+   if (m == md5) r = 8;
+   char *s = "abcdefghijklmnopqrstuvwxyz" \
+             "0123456789./" \
+             "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+   int i;
+   strcpy(obuf, "$0$");
+   obuf[1] = '0' + m;
+   for (i=0; i<r; i++)
+   {
+       obuf[i+3] = s[random() % 64];
+   }
+   obuf[r+3]='\0';
+   return 0;
+}
+
 int
 cf_crypt(char *key, char *salt, char (*encrypted)[1024])
 {
@@ -33,8 +50,14 @@ int main()
 {
    char encrypted[1024];
 
+   srand(time(NULL));
    cf_crypt("vagrant", "$6$aaaaaaaaaa", &encrypted);
    printf("[Encrypted=%s]\n", encrypted);
+
+
+   char obuf[20];
+   getrndsalt(5, obuf);
+   printf("salt=[%s]\n", obuf);
    return 0;
 }
 #endif
--- libutils/pwcrypt.h
+++ libutils/pwcrypt.h
@@ -7,6 +7,13 @@
 #define _GNU_SOURCE
 #include <crypt.h>
 
+typedef enum {
+   md5 = 1,
+   sha256 = 5,
+   sha512 = 6
+} PwHashMethod;
+
+int getrndsalt(PwHashMethod m, char *obuf);
 int cf_crypt(char *key, char *salt, char (*encrypted)[1024]);
 
 #endif
