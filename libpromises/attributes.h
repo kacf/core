a61068afcfe81432b5ea432b74356129cfe4d6ae
Mark Burgess <mark@cfengine.com>
2014-01-24 12:18:12 +0100

    Connectors for syntax to state

--- libpromises/attributes.h
+++ libpromises/attributes.h
@@ -1,16 +1,16 @@
 /*
-   Copyright (C) CFEngine AS
+  Copyright (C) CFEngine AS
 
-   This file is part of CFEngine 3 - written and maintained by CFEngine AS.
+  This file is part of CFEngine 3 - written and maintained by CFEngine AS.
 
-   This program is free software; you can redistribute it and/or modify it
-   under the terms of the GNU General Public License as published by the
-   Free Software Foundation; version 3.
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by the
+  Free Software Foundation; version 3.
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License
   along with this program; if not, write to the Free Software
@@ -53,6 +53,7 @@ Attributes GetStorageAttributes(const EvalContext *ctx, const Promise *pp);
 Acl GetAclConstraints(const EvalContext *ctx, const Promise *pp);
 ContextConstraint GetContextConstraints(const EvalContext *ctx, const Promise *pp);
 Database GetDatabaseConstraints(const EvalContext *ctx, const Promise *pp);
+Interfaces GetInterfaceConstraints(const EvalContext *ctx, const Promise *pp);
 DefineClasses GetClassDefinitionConstraints(const EvalContext *ctx, const Promise *pp);
 EditColumn GetColumnConstraints(const EvalContext *ctx, const Promise *pp);
 EditDefaults GetEditDefaults(const EvalContext *ctx, const Promise *pp);
