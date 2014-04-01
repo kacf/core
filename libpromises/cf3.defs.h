a09e27b79f89c518137434d962579cb49dbe5dbb
Mark Burgess <mark@cfengine.com>
2014-04-01 12:09:28 +0200

    Corrections to ospf config selection

--- libpromises/cf3.defs.h
+++ libpromises/cf3.defs.h
@@ -962,6 +962,14 @@ struct LinkState_
     LinkState *next;
 };
 
+typedef struct LinkOSPF_ LinkOSPF;
+
+struct LinkOSPF_
+{
+
+    LinkOSPF *next;
+};
+
 typedef struct Bridges_ Bridges;
 
 struct Bridges_
@@ -1426,12 +1434,6 @@ typedef struct
 
 typedef struct
 {
-    int rip_metric;
-    int rip_timeout;
-    char *nat_pool;
-    bool rip_splithorizon;
-    bool rip_passive;
-    char *relay_policy;
     char *gateway_interface;
     char *gateway_ip;
     bool delete_route;
