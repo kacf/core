30ce76239eaea5016d5ba2df4e4eb7b238d26874
Kristian Amlie <kristian.amlie@cfengine.com>
2015-09-03 12:00:15 +0200

    Static assert

--- libutils/misc_lib.h
+++ libutils/misc_lib.h
@@ -48,6 +48,32 @@
 # define CF_ASSERT(condition, ...) (void)(0)
 #endif
 
+/**
+ * STATIC_ASSERT(e, m)
+ * @brief Asserts a condition at compile time.
+ * @param e Expression to assert.
+ * @param m Message to print on failure. This is currently unused and is only
+ *          here for future compatibility with C++11.
+ *
+ * Taken from here:
+ * http://www.pixelbeat.org/programming/gcc/static_assert.html
+ * Licensed under the GNU All Permissive License
+ */
+#define ASSERT_CONCAT_(a, b) a##b
+#define ASSERT_CONCAT(a, b) ASSERT_CONCAT_(a, b)
+/* These can't be used after statements in c89. */
+#ifdef __COUNTER__
+  #define STATIC_ASSERT(e,m) \
+    ;enum { ASSERT_CONCAT(static_assert_, __COUNTER__) = 1/(!!(e)) }
+#else
+  /* This can't be used twice on the same line so ensure if using in headers
+   * that the headers are not included twice (by wrapping in #ifndef...#endif)
+   * Note it doesn't cause an issue when used on same line of separate modules
+   * compiled with gcc -combine -fwhole-program.  */
+  #define STATIC_ASSERT(e,m) \
+    ;enum { ASSERT_CONCAT(assert_line_, __LINE__) = 1/(!!(e)) }
+#endif
+
 
 /*
   In contrast to the standard C modulus operator (%), this gives
